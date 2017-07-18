9233e80faf7ffd00ca0b29bdd7efb5f310440452
Gurvan Renault <renault.gurvan@gmail.com>
2017-07-18 21:26:43 +0200

    Add example : The rose is red , the violet's blue

---
+++ JosephRitson/rosearered.py
@@ -0,0 +1,8 @@
+
+# From GammerGurton'sGarland, a 1784 collection of English nursery rhymes:
+
+name=raw_input('Enter your name :');
+if (("rose"is "red") and ("violet" is "blue") and ("honey" is "sweet")):
+    print ("You're so sweet");
+else:
+    print("You're as sweet as hell "+name);
